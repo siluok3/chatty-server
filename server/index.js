631b11528f4107d3a18581357d6d3ffa35889072
Kiriakos <koulispapas@gmail.com>
2018-03-19 14:34:33 +0100

    Create the graphiql server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,34 @@
 import express from 'express';
-const PORT = 8080;
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import bodyParser from 'body-parser';
+import { createServer } from 'http';
+
+import  Schema  from './data/schema';
+import  Mocks  from './data/mocks';
+
+const GRAPHQL_PORT = 8080;
+
 const app = express();
-app.listen(PORT, () => console.log(`Server is now running on http://localhost:${PORT}`));
+
+const executableSchema = makeExecutableSchema({
+    typeDefs: Schema,
+});
+addMockFunctionsToSchema({
+    schema: executableSchema,
+    mocks: Mocks,
+    preserveResolvers: true,
+});
+
+// `context` must be an object and can't be undefined when using connectors
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+    schema: executableSchema,
+}));
+
+app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+}));
+
+const graphQLServer = createServer(app);
+
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(`GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`));
